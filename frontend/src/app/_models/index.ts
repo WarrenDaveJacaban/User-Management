7958aca8ad70a13f262fdee72cd7bdf773f5fb9b
Stefan Karl M Chan <stefanchan31@gmail.com>
2025-05-17 14:10:27 +0800

    Creating Department, Employees and Workflows

--- frontend/src/app/_models/index.ts
+++ frontend/src/app/_models/index.ts
@@ -1,3 +1,7 @@
+// frontend/src/app/_models/index.ts
 export * from './account';
 export * from './role';
 export * from './alert';
+export * from './department';
+export * from './employee';
+export * from './workflow';
